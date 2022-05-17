05542ab49a83b66952dbf8936a8121af692ea63c
Aditya <34532396+AdiA32@users.noreply.github.com>
2022-05-17 16:36:52 -0700

    tabs working X0!

--- python app - streamlit/app.py
+++ python app - streamlit/app.py
@@ -5,9 +5,9 @@ import KNN_blank, KNN_w_examples, Bigram_blank # also can import your app module
 app = MultiApp()
 
 # Add all your application here
-app.add_app("Home", KNN_blank.app)
-app.add_app("Data Stats", KNN_w_examples.app)
-app.add_app("Data Stats", Bigram_blank.app)
+app.add_app("KNN Blank", KNN_blank.app)
+app.add_app("KNN with Examples", KNN_w_examples.app)
+app.add_app("Bigram Blank - easy!", Bigram_blank.app)
 
 # The main app
 app.run()
